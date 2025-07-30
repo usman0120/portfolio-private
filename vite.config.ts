eab8994d5da4e2943dc24903da9f206de23f3970
usman0120 <207910452+usman0120@users.noreply.github.com>
2025-07-30 08:54:07 +0500

    ready for deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/portfolio-private/', // Replace with your actual GitHub repo name
   build: {
     outDir: 'dist'
   }
